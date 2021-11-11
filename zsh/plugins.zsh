99c23e787b4e915fecd99ec2304cb55b28f1343e
François Ledoyen <francoisledoyen@macbook-air-de-francois.home>
2021-11-11 21:20:24 +0100

    init

---
+++ zsh/plugins.zsh
@@ -0,0 +1,18 @@
+source "$HOME/antigen.zsh"
+
+antigen bundle zsh-users/zsh-completions
+antigen bundle djui/alias-tips
+antigen bundle zsh-users/zsh-syntax-highlighting
+antigen bundle zsh-users/zsh-autosuggestions
+antigen bundle git
+antigen bundle brew
+antigen bundle pip
+antigen bundle github
+antigen bundle colorize
+antigen bundle vi-mode
+antigen 
+
+THEME=denysdovhan/spaceship-prompt
+antigen list | grep $THEME; if [ $? -ne 0 ]; then antigen theme $THEME; fi
+
+antigen apply
---
+++ zsh/prompt.zsh
@@ -0,0 +1,26 @@
+SPACESHIP_PROMPT_ORDER=(
+    time          # Time stampts section
+    user          # Username section
+    host          # Hostname section
+    dir           # Current directory section
+    git           # Git section (git_branch + git_status)
+    venv
+    hg            # Mercurial section (hg_branch  + hg_status)
+    pyenv         # Pyenv section
+    line_sep      # Line break
+    vi_mode       # Vi-mode indicator
+    jobs          # Backgound jobs indicator
+    exit_code     # Exit code section
+    #char          # Prompt character
+)
+
+bindkey -v 
+eval spaceship_vi_mode_enable
+
+SPACESHIP_VI_MODE_INSERT="❯ "
+SPACESHIP_VI_MODE_NORMAL="• "
+SPACESHIP_VI_MODE_COLOR="green"
+SPACESHIP_VI_MODE_SUFFIX=""
+
+SPACESHIP_VENV_SYMBOL="🐍 "
+SPACESHIP_VENV_COLOR="green"
