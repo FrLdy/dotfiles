66bdfb9a6ad4c7d3505b15a53bf0235e0c79cca9
François Ledoyen <francoisledoyen@macbook-air-de-francois.home>
2021-11-14 23:36:49 +0100

    Update

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,7 +1,6 @@
 source "$HOME/antigen.zsh"
 
 antigen bundle zsh-users/zsh-completions
-antigen bundle djui/alias-tips
 antigen bundle zsh-users/zsh-syntax-highlighting
 antigen bundle zsh-users/zsh-autosuggestions
 antigen bundle git
@@ -10,6 +9,7 @@ antigen bundle pip
 antigen bundle github
 antigen bundle colorize
 antigen bundle vi-mode
+antigen bundle zpm-zsh/material-colors
 antigen 
 
 THEME=denysdovhan/spaceship-prompt
